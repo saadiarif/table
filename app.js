d5575688b088472ec487716885137043620fd48a
saadiarif <saadarif723@gmail.com>
2024-05-10 17:53:23 +0500

    table

---
+++ app.js
@@ -0,0 +1,150 @@
+/*
+>
+<
+>=
+<=
+===
+!==
+*/
+
+
+
+let table= +prompt("enter table name");
+let count= 1;
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+document.write(table + " x " + count + " = " + table * count++ + "<br />");
+
+
+
+
+// let isShopOpen = confirm("is mudassir shop open ?");
+// console.log(isShopOpen);
+
+
+// let isShopOn = confirm("is mudassir shop open ?");
+// if (isShopOn) {
+//     console.log("purchase lentils");
+// }
+
+
+// let age= +prompt("what is your age ?");
+// if (age > 20) {
+//     document.write("congrats! you are eligible");
+// }
+
+
+// let domecile= prompt("In which city do you live ?");
+// if (domecile === "karachi") {
+//     console.log("Oh on! you are not eligible for goverment jobs.")
+// }
+
+// let isShopName= prompt("which shop is open?");
+// if (isShopName==="Farhan") {
+//     document.write("bring french fries");
+// }
+// else if (isShopName==="almadina") {
+//     document.write("anda wala burger");
+// }
+// else if (isShopName==="ejaaz") {
+//     document.write("1 biryani");
+// }
+// else {
+//     document.write("bring something to eat");
+// }
+
+// let saad= prompt("what is your boay weight % ?");
+// if (saad>="40%") {
+//     document.write("Cardio");
+// }
+// else if (saad>="30%") {
+//     document.write("Strenght");
+// }
+// else if (saad>="20%") {
+//     document.write("Cardio + Strenght");
+// }
+
+// let weight= +prompt("what is your weight ?");
+// let timimg= +prompt("what is your timing ?");
+// let gender= prompt("what is your gender ?");
+// let age= +prompt("what is your age ?");
+// if (weight>=100 && timimg<6 && gender==="male" || "female" && age>30) {
+//     document.write("you are eligible my sports clubs");
+// }
+
+
+// let books =['Deewan Galib','Baal Jabreel','Bahar Shariat', 'Rich Dad Poor Dad',];
+
+// let userRequiredBook = prompt('Which book do you want to read?');
+
+// let booksIndexOf = books.indexOf (userRequiredBook);
+// if (bookIndex > -1){
+//     document.write("your required book is indexed at" +bookIndex)
+// }
+// else{
+//     document.write("sorry your required book is unavailable.");
+// }
+
+
+// let primeNums=[1,2,3,5,7,11,13,17,19,23];
+// // Array.splice(startingIndex, delete item count, add new item(s))
+// primeNums.splice(2, 3, 29, 31, 37);
+// document.write(primeNums);
+// console.log(primeNums);
+
+
+// let circketers=['Saad','Talha','Taha','Shahzaib','Salam','Azan','Ali'];
+// circketers.slice(4, 6);
+// console.log(circketers)
+
+
+// let strArr=['S','a','a','d','i'];
+// document.write(strArr.join(''));
+// console.log(strArr.join(''));
+
+
+// let strArr=['c','i','v','i','c'];
+// console.log(strArr.join(''));
+// console.log(strArr.reverse().join(''));
+
+// let str='Muhammad Saadi Arif';
+// // console.log(str[1]);
+// console.log(str.length);
+// console.log(str.slice(8,14));
+
+
+// let firstName='sAaDi';
+
+// console.log(firstName.toLowerCase());
+// console.log(firstName.toUpperCase());
+
+// let userName=prompt('what is your name ?');
+// if (firstName.toLowerCase() === userName.toLowerCase()) {
+
+// }
+
+
+
+
+
+
+
+
+
